f0601446efccf5f953f55b981bfb090c5adc35fb
C-Fred <fchesneau@gmail.com>
2022-07-16 16:01:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import streamlit as st
 params = st.experimental_get_query_params()
 
 st.title("Trial redirect")
-st.text(f"Just checking. Param: {params[param]}")
+st.text(f"Just checking. Param: {params[param][0]}")
